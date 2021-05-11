2a327b6e338913b9d03c553fd89008c11fe1828e
waelbh <47089212+waelbh@users.noreply.github.com>
2021-05-11 20:55:45 +0100

    Heroku

--- src/server.ts
+++ src/server.ts
@@ -19,7 +19,7 @@ app.use(cookieParser());
 // all available routes
 app.use(express.static(path.join(__dirname, "./build")));
 
-app.get("/*", (_req, res) => {
+app.get("/", (_req, res) => {
   res.sendFile(path.join(__dirname, "./build", "index.html"));
 });
 routes(app);
